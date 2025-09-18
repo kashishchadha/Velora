1adf98eb0fd0106f95e645753a14a4419b763bf0
kashish <kashishchadha0509@gmail.com>
2025-09-18 10:34:28 +0530

    search barup

--- client/src/components/SideMenu.jsx
+++ client/src/components/SideMenu.jsx
@@ -9,8 +9,9 @@ function SideMenu() {
 
 
     <label htmlFor='' className='flex items-center gap-2 cursor-pointer'>
-        <input type="radio" name="sort" value="newest" className='appearance-none w-4 h-4 border-[1.5px] border-blue-800 cursor-pointer rounded-sm checked:bg-blue-800 '
+        <input type="radio" name="sort" value="newest" className='appearance-none w-4 h-4 border-[1.5px] border-blue-800 cursor-pointer rounded-sm checked:bg-blue-800 '/>
     </label>
+
     <h1 className='mt-8 mb-4 text-sm font-medium'>Categories</h1>
 
 <div className="flex flex-col gap-2 text-sm">
