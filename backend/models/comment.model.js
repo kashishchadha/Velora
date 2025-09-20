8fac8cb43f3a5792bce983a9a9f399ecc2c9bef4
kashish <kashishchadha0509@gmail.com>
2025-09-20 10:15:26 +0530

    databases

---
+++ backend/models/comment.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose"
+import { Schema } from "mongoose"
+const commentSchema= new Schema()(
+    {
+        desc:{
+            type:String,
+            required:true,
+           
+        },
+          user:{
+            type:Schema.Types.ObjectId,
+            ref:"User",
+            required:true,
+        },
+          user:{
+            type:Schema.Types.ObjectId,
+            ref:"Post",
+            required:true,
+        },
+
+    },{timestamps:true}
+);
+export default mongoose.model("Comment",commentSchema)
