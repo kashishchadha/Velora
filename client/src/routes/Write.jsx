8673e8afa3f0c126af85aff640fc45541d5685aa
kashish <kashishchadha0509@gmail.com>
2025-10-07 21:55:20 +0530

    little changes

--- client/src/routes/Write.jsx
+++ client/src/routes/Write.jsx
@@ -10,26 +10,7 @@ import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 
 import {IKContext,IKUpload} from "imagekitio-react"
-const authenticator = async () => {
-  try {
-    const response = await fetch(
-      `${import.meta.env.VITE_API_URL}/posts/upload-auth`
-    );
-
-    if (!response.ok) {
-      const errorText = await response.text();
-      throw new Error(
-        `Request failed with status ${response.status}: ${errorText}`
-      );
-    }
-
-    const data = await response.json();
-    const { signature, expire, token } = data;
-    return { signature, expire, token };
-  } catch (error) {
-    throw new Error(`Authentication request failed: ${error.message}`);
-  }
-};
+import Upload from '../components/Upload';
 
 
 function Write() {
@@ -74,37 +55,15 @@ const data={
 console.log(data)
 mutation.mutate(data)
 }
-const onError=(err)=>{
-  console.log(err);
-  toast.error("Image upload failed!");
-};
-const onSuccess=(res)=>{
-  console.log(res)
-  setCover(res)
- }
- const onUploadProgress=(progress)=>{
-console.log(progress);
-setProgress(Math.round(progress.loaded/progress.total)*100)
- }
+
 
   return (
     <div className='h-[calc(100vh-64px)] md:h-[calc(100vh-80px)] flex flex-col gap-6'>
       <h1 className='text-cl font-light'>Create a New Post</h1>
       <form onSubmit={handleSubmit} className='flex flex-col gap-6 flex-1 mb-6'>
-   <IKContext
-      publicKey={import.meta.env.VITE_IK_PUBLIC_KEY}
-      urlEndpoint={import.meta.env.VITE_IK_URL_ENDPOINT}
-      authenticator={authenticator}
-    >
-      <IKUpload
-        useUniqueFileName
-        onError={onError}
-        onSuccess={onSuccess}
-        onUploadProgress={onUploadProgress}
-   
-      />
-   
-    </IKContext>      
+    <Upload type="image" setProgress={setProgress} setData={setCover}>
+      <button className='w-max p-2 shadow-md rounded-xl text-sm text-gray-500 bg-white'>Add a cover image</button>
+    </Upload>
       <input className='text-4xl font-semibold bg-transparent outline-none' type="text" placeholder="My Awesome Story" name="title"/>
         <div className="flex items-center gap-4">
           <label htmlFor='' className='p-2 rounded-xl bg-white shadow-md'>Choose a category:</label>
@@ -130,7 +89,7 @@ setProgress(Math.round(progress.loaded/progress.total)*100)
           </div>
 <ReactQuill value={value} onChange={setValue} theme='snow' className='flex-1 rounded-xl bg-white shadow-md'/>
 </div>
-<button disabled={mutation.isPending} className='bg-blue-800 text-white font-medium rounded-xl m-4 p-2 w-36  disabled:bg-blue-400 disabled:cursor-not-allowed'>{mutation.isPending?"Loading...":"Send"}</button>
+<button disabled={mutation.isPending || (0<progress && progress < 100)} className='bg-blue-800 text-white font-medium rounded-xl m-4 p-2 w-36  disabled:bg-blue-400 disabled:cursor-not-allowed'>{mutation.isPending?"Loading...":"Send"}</button>
 {"Progress:"+progress}
 {mutation.isError && <span>{mutation.error.message}</span>}
       </form>
