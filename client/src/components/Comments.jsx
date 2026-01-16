d4b3d2ef3c8dab1beba28a263c69343185c229bb
kashish <kashishchadha0509@gmail.com>
2026-01-16 09:41:33 +0530

    comments in frontend handled

--- client/src/components/Comments.jsx
+++ client/src/components/Comments.jsx
@@ -1,6 +1,23 @@
 import React from 'react'
 import Comment from './Comment'
+import axios from 'axios';
+import { useQuery } from '@tanstack/react-query';
+
+const fetchComment= async (postId)=>{
+  const res=await axios.get(`${import.meta.env.VITE_API_URL}/comments/${postId}`);
+  return res.data;
+}
 function Comments() {
+  const {isPending,error,data}=useQuery({
+
+  queryKey:["comments",postId],
+  queryFn:()=>fetchComment(postId),
+})
+
+if(isPending) return "Loading"
+if(error) return "something went wrong"+error.message
+if(!data) return "comment not found"
+
   return (
     <div className='flex flex-col gap-8 lg:w-3/5'>
 <h1 className='text-xl text-gray-500 underline'>Comments</h1>
@@ -8,7 +25,11 @@ function Comments() {
     <textarea placeholder="Write a comment.." className="w-full p-4 bg-white rounded-xl"/>
     <button className='bg-blue-800 px-4 py-3 text-white font-medium rounded-xl'>Send</button>
 </div>
-<Comment/>
+
+{data.map((Comment)=>(
+<Comment key={Comment._id} Comment={Comment}/>
+))}
+
 <Comment/>
 <Comment/>
 <Comment/>
